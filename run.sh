f02d0d95728ce3feb4b3130ef6ea6f9105dd3453
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-05-29 12:24:11 +0800

    Update dockercompose

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 docker run --rm -it -p 8080:8080 alextanhongpin/go-api
+
+# docker run --rm --name demo-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+# 272f98126b556e3296cc1a75772eeb6cc382fa219b983f63c5a6441907fba55d
